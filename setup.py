36df0171c93f5be948d50ac57407441090e6bee6
Daniel <danielrotem@mail.huji.ac.il>
2020-08-19 18:45:52 +0300

    fixed changes

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-pip install requierments.txt
+from setuptools import setup, find_packages
+
+setup(
+    name='double_DQN',
+    version='0.1',
+    install_requires=[
+        'numpy>=1.17.4'
+        'tensorflow>=2.3.0'
+        'pandas>=1.1.0'
+        'keras>=2.3.1'
+        'tqdm>=4.40.1'
+    ]
+)
