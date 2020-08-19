2fe131d921e0088e0f4833116703a1302b3018e3
Daniel <danielrotem@mail.huji.ac.il>
2020-08-19 18:47:16 +0300

    fixed changes

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='double_DQN',
     version='0.1',
     install_requires=[
-        'numpy>=1.17.4'
-        'tensorflow>=2.3.0'
-        'pandas>=1.1.0'
-        'keras>=2.3.1'
+        'numpy>=1.17.4',
+        'tensorflow>=2.3.0',
+        'pandas>=1.1.0',
+        'keras>=2.3.1',
         'tqdm>=4.40.1'
     ]
 )
